bd4d627de3c456f3e280c1f4e9034da975a7be07
PatilAkashA <aka.a.patil4339@gmail.com>
2023-02-18 00:28:12 +0530

    This is my basic program

---
+++ Java Program/src/BasicJavaProgram/Maths.java
@@ -0,0 +1,18 @@
+package BasicJavaProgram;
+
+public class Maths {
+
+	public static void main(String[] args) {
+		int x=45;
+		int y=32;
+		int z=12;
+		int add_result=x+y+z;
+		int sub_result=x-y-z;
+		int mul_result=x*y*z;
+		System.out.println("add_result!!!" +add_result);
+		System.out.println("sub_result!!!" +sub_result);
+		System.out.println("mul_result!!!" +mul_result);
+
+	}
+
+}
